d6c435c950b58ed58eb17413f58f54efaa721e1e
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-06-14 18:58:34 -0300

    Function to create a service

--- src/pages/NewService.tsx
+++ src/pages/NewService.tsx
@@ -5,19 +5,48 @@ import "../styles/input-container.scss";
 import { Button } from "../components/Button";
 import { DangerButton } from "../components/DangerButton";
 import { MenuBar } from "../components/MenuBar";
+import { FormEvent, useState } from "react";
+import { database } from "../services/firebase.";
 
 export function NewService() {
   const { user } = useAuth();
+
+  const [newServiceCategory, setNewServiceCategory] = useState("");
+  const [newServicePrice, setNewServicePrice] = useState("");
+  const [newServiceDescription, setNewServiceDescription] = useState("");
+
+  async function handleCreateService(event: FormEvent) {
+    event.preventDefault();
+
+    if (
+      newServiceCategory.trim() === "" ||
+      newServicePrice.trim() === "" ||
+      newServiceDescription.trim() === ""
+    )
+      return;
+
+    const serviceRef = database.ref('services')
+    const firebaseService = await serviceRef.push({
+      category: newServiceCategory,
+      price: newServicePrice,
+      description: newServiceDescription,
+      authorId: user?.id
+    })
+  }
 
   return (
     <main id="page-new-service">
       <MenuBar></MenuBar>
       <div className="page-container">
         <h2>Criar um novo serviço</h2>
-        <form>
+        <form onSubmit={handleCreateService}>
           <div className="input-container">
             <span className="material-icons">category</span>
-            <select name="select">
+            <select
+              name="select"
+              onChange={(event) => setNewServiceCategory(event.target.value)}
+              value={newServiceCategory}
+            >
               <option value="none">Categoria</option>
               <option value="jardinagem">Jardinagem</option>
               <option value="faxina">Faxina</option>
@@ -31,6 +60,8 @@ export function NewService() {
               name="price"
               id="price"
               placeholder="Preço médio"
+              onChange={(event) => setNewServicePrice(event.target.value)}
+              value={newServicePrice}
             />
           </div>
 
@@ -41,12 +72,14 @@ export function NewService() {
               name="price"
               id="price"
               placeholder="Descrição"
+              onChange={(event) => setNewServiceDescription(event.target.value)}
+              value={newServiceDescription}
             />
           </div>
 
           <div className="buttons-container">
             <DangerButton id="cancel-button">Cancelar</DangerButton>
-            <Button>Salvar</Button>
+            <Button type="submit">Salvar</Button>
           </div>
         </form>
       </div>
